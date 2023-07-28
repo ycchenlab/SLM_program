4bf92327b5c794007c62434eb03afc89ce89615f
YanChengShih <jamesshih268@gmail.com>
2023-07-28 18:11:48 +0800

    first commit

---
+++ _main_test.py
@@ -0,0 +1,158 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 27 22:11:29 2023
+
+@author: ycche
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+from numpy.fft import fft2, ifft2
+from function_test import costfunction
+from function_test import converter
+import tensorflow as tf
+import imageio
+import time
+import os
+import shutil
+from cv2 import imread, IMREAD_GRAYSCALE
+
+
+# ===================================== Parameters config ===============================================
+
+# Loading the target image
+# Create grid
+N = 500  # Number of grid points
+L = 1  # Grid size
+x = np.linspace(-L/2, L/2, N)
+y = np.linspace(-L/2, L/2, N)
+X, Y = np.meshgrid(x, y)
+
+
+# Super-Gaussian profile parameters
+m = 10  # Super-Gaussian exponent
+A = 1  # Super-Gaussian amplitude
+w0 = L/6  # Super-Gaussian width
+
+# Generate super-Gaussian profile -- it is intensity
+target = np.square(A * np.exp(-(np.abs(X)/w0)**(2*m)) * np.exp(-(np.abs(Y)/(w0/4))**(2*m)))
+targetphase = np.zeros([N,N])
+# Gaussian beam parameters
+w0 = L/6  # Gaussian beam waist radius
+
+# Calculate Gaussian beam profile -- it is electric field amplitude
+R = np.sqrt(X**2 + Y**2)  # Circular shape
+initial_profile = np.exp(-((R/w0)**2))
+
+target /= np.max(target)  # Normalize matrix
+
+# Defining DOE phase
+
+DOE = np.random.rand(N,N)*2*np.pi
+
+s = 30
+
+# Create an empty list to store frames
+frames = []
+
+# costType: 1 = simple cost function(Ct2), 2 = smoothing neighbor pixels(Cs), 3 = alternating Ct4 / Cs, 4 = alternating Ct2 / Cs, 5 = Ct4 / Ct2
+costType = 1
+learning_rate=0.05
+
+# ===================================== Parameters config ===============================================
+
+# conjugate gradient method algorithm
+# Iterate to calculate the phase value
+for t in range(s):
+    # Start the timer
+    start_time = time.time()
+    
+    DOEphase = np.exp(1j * DOE)
+
+####################    # Forward iteration
+    iterf = fft2(initial_profile * DOEphase) # field fft
+    intf = np.square(np.abs(iterf)) / np.max(np.square(np.abs(iterf))) # normalized training intenstiy
+    angf = np.angle(iterf)
+    A = target * np.exp(1j * angf)
+
+    # Backward iteration
+    '''
+    iterb = ifft2(A)
+    angb = np.angle(iterb)
+    DOE = angb
+    '''
+    error = target - intf # Calculate error
+    E = np.sum(np.abs(error)) / (N * N)
+    differences = target - intf
+    squaredDifferences = differences**2
+    meanSquaredDifferences = np.mean(squaredDifferences)
+    
+    
+    ############################ Optimization funciton
+    
+
+    cost, DOE_tf, learning_rate, optimizer_string = costfunction(DOE, target, initial_profile, N,t,learning_rate, costType,squaredDifferences, targetphase)
+    DOE = DOE_tf.numpy() 
+
+    rmse = np.sqrt(meanSquaredDifferences)
+    
+    if E < 0.0005:
+        iteration = t
+        break
+    
+    # End the timer
+    end_time = time.time()
+    # Calculate the elapsed time
+    elapsed_time = end_time - start_time    
+
+    # Text for Optimazation   
+    text = f'Iteration: {t+1}\nLearning Rate: {learning_rate}\nRMSE: {round(rmse, 4)}\nOptimizer: {optimizer_string}\nElapsed time: {round(elapsed_time, 2)} seconds'
+    
+    # Text for GS
+    #text = f'Iteration: {t+1}\nRMSE: {round(rmse, 4)}\nElapsed time: {round(elapsed_time, 2)} seconds'
+    
+    
+    # Plot squared differences and save the figure as an image
+    plt.figure(dpi=300)
+    fig, ax = plt.subplots(figsize=(6, 6))
+    plt.imshow(intf, cmap='jet')
+    plt.axis('image')
+    plt.colorbar()
+    plt.xlabel('x coordinate')
+    plt.ylabel('y coordinate')
+    plt.title('Training Tophat')
+    plt.annotate(text, xy=(0.05, 0.8), xycoords='axes fraction', color='white', fontsize=7, weight='bold')
+    # Save the figure as an image    
+    save_path = r'C:\Users\ycche\git repo\SLM_program\tempPNG\\'
+    filename = f'plot_{t}.png'
+    plt.savefig(save_path + filename, dpi = 300)
+    # Convert the plot to an image array
+    fig = plt.gcf()
+    fig.canvas.draw()
+    frame = np.array(fig.canvas.renderer.buffer_rgba())
+    frames.append(frame)
+    plt.close()
+
+    # Read the saved image as an array and add it to the frames list
+
+    # Remove the saved image file
+
+# End of iteration
+
+
+# save DOE data for next time use
+np.save('DOE_data.npy', DOE)
+
+'''
+# Specify the source file path
+source_path = "C:/Users/ycche/git repo/SLM_program/tempPNG/plot_29.png"  # Replace with the actual path of the source file
+
+# Specify the destination directory
+destination_directory = "C:/Users/ycche/git repo/SLM_program/IntensityModifiedCostFunction/"  # Replace with the actual path of the destination directory
+
+# Copy the file
+shutil.copy(source_path, destination_directory)
+
+'''
+# Save the frames as mp4
+converter()
---
+++ function_test.py
@@ -0,0 +1,198 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 27 22:03:55 2023
+
+@author: ycche
+"""
+
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import os
+import imageio
+from datetime import datetime
+
+
+def costfunction(DOE, target, initial_profile, N, t, LR, costType, squaredDifferences,targetphase):
+ # Set device placement for TensorFlow operations
+ 
+
+    
+# turn numpy objects into tensor (preparation for deep-learning-based optimization methods)
+    complex_one = tf.constant(1j, dtype=tf.complex64)
+    DOE_tf = tf.convert_to_tensor(DOE, dtype=tf.complex64)
+    target_tf = tf.convert_to_tensor(target, dtype=tf.float32)
+    initial_profile_tf = tf.convert_to_tensor(initial_profile, dtype=tf.complex64)
+    targetphase_tf = tf.convert_to_tensor(targetphase, dtype=tf.float32)
+    
+# initialize the optimization parameters
+    DOE_tf = tf.math.real(DOE_tf)
+    variables = tf.Variable(DOE_tf)
+    learning_rate=LR
+    optimizer = tf.optimizers.Adam(learning_rate)
+
+    '''
+    intf = tf.abs(iterf) / tf.reduce_max(tf.abs(iterf)) # normalized training intensity 
+    '''
+    cost = tf.reduce_sum(squaredDifferences)
+    cost_values = []
+    plt.figure()
+    
+
+    
+    def costFnSmoothing (variables):
+        # Pad the tensor to handle boundaries
+        
+        padded_tensor = tf.pad(tf.square(tf.abs(tf.signal.fft2d(initial_profile_tf * tf.exp(complex_one * tf.cast(variables, tf.complex64)))))/ tf.reduce_max(tf.square(tf.abs(tf.signal.fft2d(initial_profile_tf * tf.exp(complex_one * tf.cast(variables, tf.complex64)))))), [[2, 2], [2, 2]])  # Pad with 2 extra rows and columns
+
+        input_tensor = tf.constant(padded_tensor)  # Your 500x500 tensor
+
+        # Select the center element and adjacent elements
+        center_elements = input_tensor[1:-1, 1:-1]
+        up_elements = input_tensor[:-2, 1:-1]
+        down_elements = input_tensor[2:, 1:-1]
+        left_elements = input_tensor[1:-1, :-2]
+        right_elements = input_tensor[1:-1, 2:]
+        
+        
+        
+        # Calculate the squared difference between center element and adjacent elements
+        squaredDiff = tf.square(tf.square(center_elements) - tf.square(up_elements)) + tf.square(tf.square(center_elements) - tf.square(down_elements)) + tf.square(tf.square(center_elements) - tf.square(left_elements)) + tf.square(tf.square(center_elements) - tf.square(right_elements))
+        
+        # Sum up the squared differences for all elements
+           
+        smoothnessInfo = tf.reduce_sum(squaredDiff)
+        # smoothnessInfo += feature_difference
+        return smoothnessInfo, squaredDiff
+    
+    def conjugategrad (variables):
+        d = 3
+        DOEphase = tf.exp(complex_one * tf.cast(variables, tf.complex64))
+        iterf_tf = tf.signal.fft2d(initial_profile_tf *DOEphase) # training output electric field
+        intf_tf = tf.math.square(tf.abs(iterf_tf)) / tf.math.reduce_max(tf.square(tf.abs(iterf_tf))) # normalized training intenstiy
+        angf_tf = tf.math.angle(iterf_tf)
+        
+        #print(variables)
+        costfun = 10**d*(1-tf.reduce_sum(tf.sqrt(tf.math.multiply(intf_tf, target_tf))*tf.cos(targetphase_tf-angf_tf)))
+        
+        return costfun
+    
+    def costFnEfficient (variables):
+        
+        
+        return something
+    
+
+    def costFnSimple (variables):
+        costfuns = tf.reduce_sum(tf.pow(target_tf - tf.abs(tf.square(
+            tf.signal.fft2d(initial_profile_tf * tf.exp(complex_one * tf.cast(variables, tf.complex64)))))/ 
+            tf.reduce_max(tf.square(tf.abs(tf.signal.fft2d(initial_profile_tf * tf.exp(complex_one * 
+            tf.cast(variables, tf.complex64)))))), pp))
+        #print(variables)
+        return costfuns
+
+    num_iterations = 30
+    for i in range(num_iterations):
+    
+        # Compute the gradient using tf.GradientTape
+        with tf.GradientTape() as tape:
+            tape.watch(variables)
+        
+            # 1 = simple cost function(Ct2),           
+            if costType==1:
+                pp = 2
+                cost = costFnSimple(variables)
+                    
+            # 2 = smoothing neighbor pixels(Cs),            
+            elif costType==2:
+                cost, squaredDiff = costFnSmoothing(variables)
+                             
+             # 3 = alternating Ct4 / Cs,      
+            elif costType==3:
+                if i%2 == 1:
+                    pp = 4    
+                    cost = costFnSimple(variables)               
+                else:
+                    cost, squaredDiff = costFnSmoothing(variables)
+
+            # 4 = alternating Ct2 / Cs, 
+            elif costType==4:
+                if i%2 == 1:
+                    pp = 4    
+                    cost = costFnSimple(variables)               
+                else:
+                    cost, squaredDiff  = costFnSmoothing(variables)
+
+            # 5 = Ct4 / Ct2
+            elif costType==5:
+                if i%2 == 1:
+                    pp = 4    
+                    cost = costFnSimple(variables)               
+                else:
+                    pp = 2    
+                    cost = costFnSimple(variables)   
+                    
+            elif costType==6:
+                cost = conjugategrad(variables)
+                        
+            gradients = tape.gradient(cost, variables)
+            gradients = tf.reshape(gradients,(N,N))
+            print(gradients)
+            
+    # Perform optimization
+        optimizer.apply_gradients([(gradients, variables)])
+        cost_values.append(cost.numpy())
+        print (cost.numpy())
+        
+        
+    optimizer_string = str(optimizer)
+    
+    plt.plot(range(1, num_iterations + 1), cost_values, )
+    plt.xlabel('Iteration')
+    plt.ylabel('Cost')
+    plt.title('Cost Function')
+    plt.annotate(f'Learning Rate: {learning_rate}\nIteration: {t+1}', xy=(0.05, 0.9), xycoords='axes fraction')
+    plt.show()
+
+    return cost, variables, learning_rate, optimizer_string
+
+
+def get_file_creation_time(file_path):
+    # Get the creation time of a file
+    timestamp = os.path.getmtime(file_path)
+    return datetime.fromtimestamp(timestamp)
+
+
+def converter():
+        # Folder path containing the images
+    folder_path = 'C:/Users/ycche/git repo/SLM_program/tempPNG'
+    
+    # List to store image file names
+    image_files = []
+    
+    # Iterate through the files in the folder
+    for file in os.listdir(folder_path):
+        if file.startswith('plot_') and file.endswith('.png'):
+            image_files.append(os.path.join(folder_path, file))
+    
+    # Sort the image files based on creation time
+    image_files.sort(key=get_file_creation_time)
+    
+    # Create an empty list to store frames
+    frames = []
+    
+    # Read the images and add them to the frames list
+    for image_file in image_files:
+        frame = imageio.imread(image_file)
+        frames.append(frame)
+    
+    # Set the file path and name for the output video
+    output_path = 'output.mp4'
+    
+    writer = imageio.get_writer(output_path, format='mp4', mode='I', fps=10)
+    
+    # Write the frames to the video
+    for frame in frames:
+        writer.append_data(frame)
+    
+    # Close the writer
+    writer.close()
